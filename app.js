d35f01690c3afbcb535e269c8ba2655d42304185
Deepak Kumrawat <deepakkumrawat8@gmail.com>
2018-08-10 13:43:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const request=require('request');
+const axios=require('axios');
+const yargs=require('yargs');
+
+
+const argv= yargs.argv
+
+console.log(argv);
+
+request({
+    url:`http://api.openweathermap.org/data/2.5/weather?appid=5fd5e9f589b704c2197465649c3c6e93&q=${argv.city}`,
+    json:true
+    }, function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body: ',body);
+//  console.log(`address: ${body.results[0].formatted_address}`);
+    if(error){
+        console.log('Unable to connect to openWeatherMap server');
+    }
+    elseif(body.cod==='404'){
+        console.log('city not found!');
+    }
+    else if(body.cod===200){
+        console.log(`latitude: ${body.coord.lat}`);
+        console.log(`longitude: ${body.coord.lon}`);
+        console.log(`temprature: ${body.main.temp}K`);
+        console.log(`weather: ${body.weather[0].main}`);
+    }
+});
